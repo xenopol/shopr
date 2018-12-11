e951f77291c5be4e293a624dfa2455d95782500d
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2018-12-11 20:10:58 +0100

    Add new list item

--- src/store/reducers/shoppingList.js
+++ src/store/reducers/shoppingList.js
@@ -1,4 +1,5 @@
 import { TOGGLE_CHECK } from '../actions/listItem'
+import { ADD_NEW_LIST_ITEM } from '../actions/addNewListItem';
 
 const initialState = [
   {
@@ -12,6 +13,8 @@ const shoppingList = (state = initialState, { type, payload }) => {
   switch (type) {
     case TOGGLE_CHECK:
       return state.map(item => (item.id === payload ? { ...item, isChecked: !item.isChecked } : item))
+    case ADD_NEW_LIST_ITEM:
+      return [...state, { id: state.length + 1, name: payload, isChecked: false }]
     default:
       return state
   }
