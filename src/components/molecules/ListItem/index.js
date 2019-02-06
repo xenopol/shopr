70f3b0942e96b81fb8529dac70286dcad62a4c84
Dan-Traian Roman <dan-traian@dan-traians-air.home>
2019-02-06 20:48:55 +0100

    crud operations

--- src/components/molecules/ListItem/index.js
+++ src/components/molecules/ListItem/index.js
@@ -1,24 +1,89 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 import Checkbox from '../../atoms/Checkbox'
+import {
+  removeListItem as removeListItemAction,
+  editListItem as editListItemAction,
+  toggleEditingListItem as toggleEditingListItemAction,
+} from '../../../store/actions/addNewListItem'
 
-const ListItem = ({ id, name, isChecked, toggleCheck }) => (
-  <div>
-    <Checkbox id={id} isChecked={isChecked} toggleCheck={toggleCheck} />
-    {name}
-  </div>
-)
+
+const styles = {
+  removeButton: {
+    marginLeft: 20,
+  },
+}
+
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.editInput = React.createRef()
+  }
+
+  handleEditItem= ({ key }, id) => {
+    const { editListItem, toggleEditingListItem } = this.props
+
+    if (key === 'Enter') {
+      editListItem({ id, name: this.editInput.current.value })
+      toggleEditingListItem({ id, isEditing: false })
+    }
+  }
+
+  handleDoubleClick= (id) => {
+    const { toggleEditingListItem } = this.props
+    toggleEditingListItem({ id, isEditing: true })
+  }
+
+  render() {
+    const { id, name, isChecked, isEditing, toggleCheck, removeItem } = this.props
+    return (
+      <div>
+        <Checkbox id={id} isChecked={isChecked} toggleCheck={toggleCheck} />
+        { isEditing
+          ? (
+            <input
+              ref={this.editInput}
+              type="text"
+              defaultValue={name}
+              autoFocus // eslint-disable-line jsx-a11y/no-autofocus
+              onKeyPress={(event) => { this.handleEditItem(event, id) }}
+            />
+          )
+          : <p onDoubleClick={() => { this.handleDoubleClick(id) }}>{name}</p>
+        }
+        <div // eslint-disable-line
+          style={styles.removeButton}
+          onClick={() => { removeItem(id) }}
+        >
+          X
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  removeItem: id => dispatch(removeListItemAction(id)),
+  editListItem: itemData => dispatch(editListItemAction(itemData)),
+  toggleEditingListItem: itemData => dispatch(toggleEditingListItemAction(itemData)),
+})
 
 ListItem.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   isChecked: PropTypes.bool,
+  isEditing: PropTypes.bool,
   toggleCheck: PropTypes.func.isRequired,
+  editListItem: PropTypes.func.isRequired,
+  removeItem: PropTypes.func.isRequired,
+  toggleEditingListItem: PropTypes.func.isRequired,
 }
 
 ListItem.defaultProps = {
   isChecked: false,
+  isEditing: false,
 }
 
-export default ListItem
+export default connect(null, mapDispatchToProps)(ListItem)
--- src/store/reducers/shoppingList.js
+++ src/store/reducers/shoppingList.js
@@ -1,14 +1,19 @@
 import { TOGGLE_CHECK } from '../actions/listItem'
-import { ADD_NEW_LIST_ITEM } from '../actions/addNewListItem'
+import {
+  ADD_NEW_LIST_ITEM,
+  REMOVE_LIST_ITEM,
+  EDIT_LIST_ITEM,
+  TOGGLE_EDITING_LIST_ITEM,
+} from '../actions/addNewListItem'
 
 const initialState = [
   {
     id: 1,
     name: 'milk',
     isChecked: false,
+    isEditing: false,
   },
 ]
-
 
 const shoppingList = (state = initialState, { type, payload }) => {
   switch (type) {
@@ -16,6 +21,15 @@ const shoppingList = (state = initialState, { type, payload }) => {
       return state.map(item => (item.id === payload ? { ...item, isChecked: !item.isChecked } : item))
     case ADD_NEW_LIST_ITEM:
       return [...state, { id: state.length + 1, name: payload, isChecked: false }]
+    case REMOVE_LIST_ITEM:
+      return state.filter(({ id }) => id !== payload)
+    case EDIT_LIST_ITEM:
+      return state.map(item => (item.id === payload.id ? { ...item, name: payload.name } : item))
+    case TOGGLE_EDITING_LIST_ITEM:
+      return state.map(item => (item.id === payload.id
+        ? { ...item, isEditing: payload.isEditing }
+        : { ...item, isEditing: false }
+      ))
     default:
       return state
   }
