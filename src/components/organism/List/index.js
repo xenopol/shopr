8bc3c5275429b8daa1270201338642eefcd4177a
Daniel Lupascu <daniellupascu@Daniels-MacBook-Pro.local>
2019-02-13 18:45:57 +0100

    Add an input field in list component for adding elements in the list

--- src/components/organism/List/index.js
+++ src/components/organism/List/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import ListItem from '../../molecules/ListItem'
 import { toggleCheck as toggleCheckAction } from '../../../store/actions/listItem'
+import { addNewListItem as addNewListItemAction } from '../../../store/actions/addNewListItem'
 
 
 const style = {
@@ -16,13 +17,48 @@ const style = {
   boxShadow: '.5rem .5rem .4rem -.3rem #0000004D',
 }
 
-const List = ({ shoppingList, toggleCheck }) => (
-  <div className="List" style={style}>
-    {shoppingList.map(({ id, name, isChecked, isEditing }) => (
-      <ListItem id={id} toggleCheck={toggleCheck} key={id} name={name} isChecked={isChecked} isEditing={isEditing} />
-    ))}
-  </div>
-)
+class List extends Component {
+  state = { inputValue: '' }
+
+  handleNewListItem = ({ keyCode }, addNewListItem) => {
+    const { inputValue } = this.state
+    if (keyCode === 13 && inputValue.trim() !== '') {
+      addNewListItem(inputValue)
+      this.setState({ inputValue: '' })
+    }
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ inputValue: value })
+  }
+
+  render() {
+    const { inputValue } = this.state
+    const { shoppingList, toggleCheck, addNewListItem } = this.props
+    return (
+      <div className="List" style={style}>
+        {shoppingList.map(({ id, name, isChecked, isEditing }) => (
+          <ListItem
+            id={id}
+            toggleCheck={toggleCheck}
+            key={id}
+            name={name}
+            isChecked={isChecked}
+            isEditing={isEditing}
+          />
+        ))}
+
+        <input
+          type="text"
+          value={inputValue}
+          placeholder="Add new item"
+          onKeyDown={e => this.handleNewListItem(e, addNewListItem)}
+          onChange={this.handleChange}
+        />
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = ({ shoppingList }) => ({
   shoppingList,
@@ -30,6 +66,7 @@ const mapStateToProps = ({ shoppingList }) => ({
 
 const mapDispatchToProps = dispatch => ({
   toggleCheck: id => dispatch(toggleCheckAction(id)),
+  addNewListItem: itemName => dispatch(addNewListItemAction(itemName)),
 })
 
 List.propTypes = {
@@ -39,6 +76,7 @@ List.propTypes = {
     isChecked: PropTypes.bool,
   })).isRequired,
   toggleCheck: PropTypes.func.isRequired,
+  addNewListItem: PropTypes.func.isRequired,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(List)
